43ca0d8fb2f8beb243de728325be80c56e05662e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-05 17:32:30 +0530

    Add files via upload

--- mbot.sh
+++ mbot.sh
@@ -1,3 +1,3 @@
-wget -O a.sh https://gist.githubusercontent.com/rooted-cyber/cd7d513396157c13a6b4b9c6b89f9c54/raw/5ce7ac679d7fead40cb93875922b090abb7d21cd/demirr
+wget -O a.sh https://gist.githubusercontent.com/rooted-cyber/19c338601c96ec9ba1cfc1a4f3eb2f5f/raw/65dab0e2835339c441531de0f3ab25ed30d3c704/mirr
 chmod 777 a.sh
 ./a.sh
