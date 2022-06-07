4f3213438ba04b9b75d7d7a3086aad7eafcf1975
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-07 17:39:22 +0530

    Update mbot.sh

--- mbot.sh
+++ mbot.sh
@@ -1,3 +1,4 @@
+pip install megasdkrestclient
 wget -O a.sh https://gist.githubusercontent.com/rooted-cyber/2996bb627f1f8ee144d3e3104ca421b7/raw/7c6294ef2a1138ca58d78ec70e1ceb6a985094a1/mirror
 chmod 777 a.sh
 ./a.sh
