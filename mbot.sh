a054b150ab670a134c5dedb490085945d20b245c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-08 07:36:33 +0530

    Update mbot.sh

--- mbot.sh
+++ mbot.sh
@@ -1,7 +1,11 @@
 #wget -O req.sh https://gist.githubusercontent.com/rooted-cyber/aab363b8a853e7eb0eb0343c82ad0abd/raw/751d857d9979e30e5a872cf0dc8af73a0633d638/req
 #bash req.sh
 #git clone -b heroku https://github.com/anasty17/mirror-leech-telegram-bot
-cd m*t*t
+#cd m*t*t
+wget -O ar.txt https://raw.githubusercontent.com/harshpreets63/Mirror-Bot/master/requirements.txt
+pip3 install -r ar.txt
+git clone https://github.com/harshpreets63/Mirror-Bot
+cd M*t
 wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cn
 wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
 ls
