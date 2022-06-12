d02ed1518e4a26ae6d6fa666f8d6f3eeed6b20bd
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-12 07:36:20 +0530

    Update mbot.sh

--- mbot.sh
+++ mbot.sh
@@ -1,22 +1,10 @@
-#wget -O req.sh https://gist.githubusercontent.com/rooted-cyber/aab363b8a853e7eb0eb0343c82ad0abd/raw/751d857d9979e30e5a872cf0dc8af73a0633d638/req
-#bash req.sh
-#git clone -b heroku https://github.com/anasty17/mirror-leech-telegram-bot
-#cd m*t*t
+wget -O req.sh https://gist.githubusercontent.com/rooted-cyber/aab363b8a853e7eb0eb0343c82ad0abd/raw/751d857d9979e30e5a872cf0dc8af73a0633d638/req
+bash req.sh
+git clone https://github.com/anasty17/mirror-leech-telegram-bot
+cd m*t*t
 pip install --upgrade pip
-wget -O ar.txt https://raw.githubusercontent.com/harshpreets63/Mirror-Bot/master/requirements.txt
-pip3 install -r ar.txt
-pip3 install psycopg2-binary
-pip install fsplit
-pip3 install fsplit
-pip3 uninstall pyrogram -y
-pip3 install -U pyrogram
-pip2 install fsplit
-pip3 install yt_dlp
-git clone https://github.com/https-github-com-rooted-cyber/Mirror-Bot-ha
-cd M*t*ha
-wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cnhm
+wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cn
 wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
 ls
 chmod 777 -R *
-pip3 install megasdkrest
 bash start.sh
