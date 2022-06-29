43a2892d27887d2b74debad8acfdfb93a85c8161
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-29 15:33:37 +0530

    Add files via upload

--- mbot.sh
+++ mbot.sh
@@ -1,14 +1,8 @@
-wget -O req.sh https://gist.githubusercontent.com/rooted-cyber/aab363b8a853e7eb0eb0343c82ad0abd/raw/751d857d9979e30e5a872cf0dc8af73a0633d638/req
-bash req.sh
-git clone https://github.com/anasty17/mirror-leech-telegram-bot
-cd m*t*t
-pip install --upgrade pip
+git clone https://github.com/arshsisodiya/helios-mirror
+cd helios*
 wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cn
 wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
-#ls
 chmod 777 -R *
-cp -Rf qBit*/conf*/qBit* /app/m*t*t
-#pwd
-#ls
-#wget https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/heroku/heroku.yml
+pip install --upgrade pip
+pip3 install -r req*s.txt
 bash start.sh
